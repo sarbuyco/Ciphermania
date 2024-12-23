104c778a67e058800f5db5751987db1a5ecd61d1
Glitch (glitch-hello-node) <none>
2024-12-23 09:45:52 +0000

    🏙🏃 Checkpoint ./server.js:70059469/4 ./public/style.css:70059379/178 ./public/index.html:70059379/38 ./public/encoder.html:70059379/1297

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const path = require('path');
 const app = express();
 
 //Serves static files inside the public folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname + '/public')));
 app.set('view engine', 'hbs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
