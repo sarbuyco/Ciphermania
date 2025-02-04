02057094d9135e7200e96cae300cbf416ec1a22a
Glitch (glitch-hello-node) <none>
2025-02-04 16:44:21 +0000

    🎑👠 Checkpoint ./public/client.js:70059469/3693 ./public/decoder.html:70059469/50

--- public/client.js
+++ public/client.js
@@ -304,3 +304,113 @@ function MorseEncode() {
         console.log(strtext);
         document.getElementById("encodeoutput").innerHTML = strtext;
       }
+
+function MorseDecode() {
+        //Get the values of the input and the shift key
+        var text = document.getElementById("decodeinput").value + "";
+        console.log(text);
+        //Shift key can go negative. This is to make sure it is equal to the normal positive shift key
+        //splitting the characters into an array
+        const arr = text.split(" ");
+        console.log(arr);
+        //defining the alphabet that the input array will be compared to
+        var upperalphabet = [
+          "A","B","C","D","E","F","G","H","I","J","K",
+          "L","M","N","O","P","Q","R","S","T","U","V",
+          "W","X","Y","Z"
+        ];
+        var specialchars = [
+          "1","2","3","4","5","6","7","8","9","0",
+          ",",".","!","?","@","&","(",")","'",'"',
+          "-","+","=","/",":"
+        ];
+        var morsealpha = [
+          ".-",
+          "-...",
+          "-.-.",
+          "-..",
+          ".",
+          "..-.",
+          "--.",
+          "....",
+          "..",
+          ".---",
+          "-.-",
+          ".-..",
+          "--", 
+          "-.",
+          "---", 
+          ".--.",
+          "--.-",
+          ".-.",
+          "...",
+          "-",
+          "..-",
+          "...-",
+          ".--",
+          "-..-",
+          "-.--",
+          "--..",
+          ".----", //start of numbers
+          "..---",
+          "...--",
+          "....-",
+          ".....",
+          "-....",
+          "--...",
+          "---..",
+          "----.",
+          "-----",
+          "--..--", //start of special charas
+          ".-.-.-",
+          "-.-.--",
+          "..--..",
+          ".--.-.",
+          ".-...",
+          "-.--.",
+          "-.--.-",
+          ".----.",
+          ".-..-.",
+          "-....-",
+          ".-.-.",
+          "-...-",
+          "-..-.",
+          "---...",
+          "/" //space
+        ];
+        
+        //bool variable to check if the character is a letter or special character
+        var check = false;
+        //The variable to be outputted
+        var strtext = "";
+
+        for (var i = 0; i < arr.length; i++) {
+          //reset the check variable back to false
+          check = false;
+          //checks if the array character is an uppercase letter, and if it is, substitutes another letter by adding the shift key.
+          for (var j = 0; j < morsealpha.length - 26; j = j + 1) {
+            if (arr[i] == morsealpha[j]) {
+              //modulo by 26 so that if j + key is more than the alphabet array length, then it will go back to index zero.
+              strtext = strtext + upperalphabet[j];
+              //if it is equal to the uppercase letter, check will be true
+              check = true;
+            }
+          }
+          //if it is still not identified as either lowercase or uppercase letter, then it is deemed a special character or a space, and is added into the string as itself.
+          if (check == false) {
+             for (var j = 26; j < morsealpha.length - 1; j = j + 1) {
+              if (arr[i] == morsealpha[j]) {
+                strtext = strtext + specialchars[j - 26];
+                check = true;
+              }
+            }
+          }
+          
+          if (check == false) {
+            strtext = strtext + " ";
+          }
+        }
+        //outputs the string
+        console.log(strtext);
+        document.getElementById("decodeoutput").innerHTML = strtext;
+      }
