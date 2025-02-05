28781369112d0e240099847d6ee3c9b76bed11f9
Glitch (glitch-hello-node) <none>
2025-02-05 04:55:54 +0000

    🌼🌅 Checkpoint ./public/client.js:70059469/3100 ./public/decoder.html:70059469/46

--- public/client.js
+++ public/client.js
@@ -794,3 +794,90 @@ function SMSEncode() {
         console.log(strtext);
         document.getElementById("encodeoutput").innerHTML = strtext;
       }
+
+function SMSDecode() {
+        //Get the values of the input and the shift key
+        var text = document.getElementById("decodeinput").value + "";
+        console.log(text);
+        //Shift key can go negative. This is to make sure it is equal to the normal positive shift key
+        //splitting the characters into an array
+        const arr = text.split(" ");
+        console.log(arr);
+        //defining the alphabet that the input array will be compared to
+        var loweralphabet = [
+          "a","b","c","d","e","f","g","h","i","j","k",
+          "l","m","n","o","p","q","r","s","t","u","v",
+          "w","x","y","z"
+        ];
+        var upperalphabet = [
+          "A","B","C","D","E","F","G","H","I","J","K",
+          "L","M","N","O","P","Q","R","S","T","U","V",
+          "W","X","Y","Z"
+        ];
+        var SMSalpha = [
+          "2",
+          "22",
+          "222",
+          "3",
+          "33",
+          "333",
+          "4",
+          "44",
+          "444",
+          "5",
+          "55",
+          "555",
+          "6",
+          "66",
+          "666",
+          "7",
+          "77",
+          "777",
+          "7777",
+          "8",
+          "88",
+          "888",
+          "9",
+          "99",
+          "999",
+          "9999"
+        ];
+        
+        //bool variable to check if the character is a letter or special character
+        var check = false;
+        //The variable to be outputted
+        var strtext = "";
+
+        for (var i = 0; i < arr.length; i++) {
+          //reset the check variable back to false
+          check = false;
+          //checks if the array character is an uppercase letter, and if it is, substitutes another letter by adding the shift key.
+          for (var j = 0; j < SMSalpha.length; j = j + 1) {
+            if (arr[i] == SMSalpha[j]) {
+              //modulo by 26 so that if j + key is more than the alphabet array length, then it will go back to index zero.
+              strtext = strtext + upperalphabet[j];
+              //if it is equal to the uppercase letter, check will be true
+              check = true;
+            }
+          }
+          //if character has not been identified as an uppercase letter, then it will start to browse through the lowercase letters, following the same tactic.
+          if (check == false) {
+            for (var j = 0; j < loweralphabet.length; j = j + 1) {
+              if (arr[i] == loweralphabet[j]) {
+                strtext = strtext + SMSalpha[j];
+                check = true;
+              }
+            }
+          }
+          //if it is still not identified as either lowercase or uppercase letter, then it is deemed a special character or a space, and is added into the string as itself.
+          if (check == false) {
+            if (arr[i] == "0") {
+              strtext = strtext + " ";
+            }
+          }
+          
+        }
+        //outputs the string
+        console.log(strtext);
+        document.getElementById("decodeoutput").innerHTML = strtext;
+      }
